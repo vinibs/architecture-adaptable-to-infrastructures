8485e2014f6d73c7c83b5bc93a74a638d29354c6
Vinicius Baroni Soares <hiviniciusbs@gmail.com>
2024-03-05 23:23:17 -0300

    fix: wrong endpoint function name

--- server.py
+++ server.py
@@ -11,5 +11,5 @@ def get_legendaries():
     return poke_data_collector.collect_legendaries()
 
 @app.get("/starters/{gen_no}")
-def read_item(gen_no: int):
+def get_starters(gen_no: int):
     return poke_data_collector.collect_starter_from_generation(gen_no)
